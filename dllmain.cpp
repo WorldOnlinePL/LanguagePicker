40c0fd813c7cc448337d8d6452ec17369df19728
Milan Suwała <metonator@gmail.com>
2021-02-13 11:44:54 +0100

    Final Form

--- dllmain.cpp
+++ dllmain.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <algorithm>
 #include <filesystem>
+#include <string>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -17,21 +18,29 @@ LPSTR GetPathUnderModuleA(HMODULE hModule, LPSTR OutBuf, LPCSTR FileName) {
     return OutBuf;
 }
 
+inline std::string trim(const std::string& s)
+{
+    auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c) {return std::isspace(c); });
+    auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c) {return std::isspace(c); }).base();
+    return (wsback <= wsfront ? std::string() : std::string(wsfront, wsback));
+}
+
 void AttachProcess() {
     //Read self-ini file.
     char get_full_path[MAX_PATH];
-    char current_lang_selected[MAX_PATH];
+    char current_lang_selected[3]; 
 
     GetPathUnderModuleA(HINST_THISCOMPONENT, get_full_path, "LangPicker.ini");
-    GetPrivateProfileStringA("GameLauncher", "Language", "EN", current_lang_selected, 256, get_full_path);
+    GetPrivateProfileStringA("GameLauncher", "Language", "EN", current_lang_selected, sizeof(current_lang_selected), get_full_path);
 
-    const char* default_languages[11] = { "EN", "DE", "ES", "FR", "PL", "RU", "PT", "TC", "SC", "TH", "TR" };
-    if (std::find(std::begin(default_languages), std::end(default_languages), current_lang_selected) == std::end(default_languages)) {
-        if (current_lang_selected == "IT") {
-            if (fs::exists("../LANGUAGES/Italian_Global.bin")) {
-                fs::remove("../LANGUAGES/English_Global.bin");
-                fs::copy_file("../LANGUAGES/Italian_Global.bin", "../LANGUAGES/English_Global.bin");
-            }
+    std::string language_picked(current_lang_selected);
+
+    if (trim(language_picked) == "IT") {
+        if (fs::exists("../LANGUAGES/Italian_Global.bin")) {
+            fs::remove("../LANGUAGES/English_Global.bin");
+            fs::copy_file("../LANGUAGES/Italian_Global.bin", "../LANGUAGES/English_Global.bin");
+        } else {
+            MessageBoxA(nullptr, "Failed to load Italian LanguagePack", "WorldOnlinePL", MB_OK);
         }
     }
 
