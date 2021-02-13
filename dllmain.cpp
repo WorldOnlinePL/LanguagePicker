2845dc9dad2a7208cec851bf6c0c8b450e9d9d55
Milan Suwała <metonator@gmail.com>
2021-02-13 02:30:03 +0100

    LangPicker.ini usage

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,12 +7,26 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+extern "C" IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+
+LPSTR GetPathUnderModuleA(HMODULE hModule, LPSTR OutBuf, LPCSTR FileName) {
+    GetModuleFileNameA(hModule, OutBuf, MAX_PATH);
+    CHAR* dirend = strrchr(OutBuf, '\\') + 1;
+    strcpy_s(dirend, MAX_PATH, FileName);
+    return OutBuf;
+}
+
 void AttachProcess() {
-    const char* current_lang_selected = "IT"; //Hardcoded
+    //Read self-ini file.
+    char get_full_path[MAX_PATH];
+    char current_lang_selected[MAX_PATH];
+
+    GetPathUnderModuleA(HINST_THISCOMPONENT, get_full_path, "LangPicker.ini");
+    GetPrivateProfileStringA("GameLauncher", "Language", "EN", current_lang_selected, 256, get_full_path);
+
     const char* default_languages[11] = { "EN", "DE", "ES", "FR", "PL", "RU", "PT", "TC", "SC", "TH", "TR" };
     if (std::find(std::begin(default_languages), std::end(default_languages), current_lang_selected) == std::end(default_languages)) {
-
-        //TODO: Better way to find which file is which. Atm... Hardcoded
         if (current_lang_selected == "IT") {
             if (fs::exists("../LANGUAGES/Italian_Global.bin")) {
                 fs::remove("../LANGUAGES/English_Global.bin");
