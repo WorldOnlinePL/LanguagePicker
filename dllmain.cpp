e00b4a65e154a0dce5240578f75d3cd9ac4408aa
Milan Suwała <metonator@gmail.com>
2021-02-13 01:36:14 +0100

    Remove non-working code

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,30 +7,7 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-#define GetRegistryDword GetRegistryDwordA
-#define GetNFSWRegOptionDwordA(OptionName, Default) GetRegistryDwordA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Electronic Arts\\Need For Speed World\\", OptionName, Default)
-
-DWORD GetRegistryDwordA(HKEY hKey, LPCSTR lpSubKey, LPCSTR lpValueName, DWORD dwDefault)
-{
-	DWORD dwResult = dwDefault;
-	DWORD dwSize = sizeof(DWORD);
-	DWORD regdword = REG_DWORD;
-
-	LSTATUS lResult = 0;
-	HKEY hTargetKey;
-	if ((lResult = RegOpenKeyExA(hKey, lpSubKey, 0, KEY_READ, &hTargetKey)) == ERROR_SUCCESS) {
-		RegQueryValueExA(hTargetKey, lpValueName, NULL, &regdword, (LPBYTE)&dwResult, &dwSize);
-		RegCloseKey(hTargetKey);
-	}
-
-	return dwResult;
-}
-
-
 void AttachProcess() {
-    //TODO - Find Settings.ini used by launcher
-    char LauncherPos = GetNFSWRegOptionDwordA("LaunchInstallDir", 8123);
-
     const char* current_lang_selected = "IT"; //Hardcoded
     const char* default_languages[11] = { "EN", "DE", "ES", "FR", "PL", "RU", "PT", "TC", "SC", "TH", "TR" };
     if (std::find(std::begin(default_languages), std::end(default_languages), current_lang_selected) == std::end(default_languages)) {
